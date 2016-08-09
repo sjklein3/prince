e7b7b56e00daddfa53ee5811e0ff772422e1d194
sjklein3 <sjklein3@gmail.com>
2016-08-08 21:19:10 -0400

    Create princelyrics.js

---
+++ princelyrics.js
@@ -0,0 +1,13 @@
+function getRandomName()
+{
+    var names = [
+        'Act your age, mama (not your shoe size)',
+        'Dream if you can a courtyard; an ocean of violets in bloom; animals strike curious poses; They feel the heat; The heat between me and you',
+        'Girl, you got an ass like I've never seen; And the ride, I say the ride is so smooth; You must be a limosine',
+        'I just want your creamy thighs'
+    ];
+
+    return names[Math.floor(Math.random() * names.length)];
+}
+
+document.write( getRandomName() );
